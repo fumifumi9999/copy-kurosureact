de8f0a527a8d34ab7fefb6a6c2dae16e508e6f02
Fumi <fumi9999@gmail.com>
2023-12-03 15:52:22 +0900

    コメント挿入

--- src/app/Search.jsx
+++ src/app/Search.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react';
 
+// Search関数コンポーネントをエクスポートします。onSearchは親コンポーネントからのpropsです。
 export default function Search({ onSearch }) {
+  // keywordの初期値を空文字列に設定し、setKeyword関数で更新します。
   const [keyword, setKeyword] = useState('');
 
+  // フォームの送信を処理する関数です。
   const handleSubmit = (event) => {
+    // フォームのデフォルトの送信動作を防ぎます。
     event.preventDefault();
+    // onSearch関数を呼び出し、keywordを引数として渡します。
     onSearch(keyword);
   };
 
+  // keywordの値を更新する関数です。
   const handleKeywordChange = (event) => {
+    // イベントのターゲットから値を取得し、setKeyword関数でkeywordを更新します。
     setKeyword(event.target.value);
   };
 
+  // フォームをレンダリングします。
   return (
+    // フォームの送信時にhandleSubmit関数を呼び出します。
     <form onSubmit={handleSubmit} className="mb-8 w-1/2 mx-auto">
+      // テキスト入力フィールドの値が変更されたときにhandleKeywordChange関数を呼び出します。
       <input type="text" name="keyword" placeholder='画像を探す' onChange={handleKeywordChange} className="input input-bordered w-full" />
+      // 送信ボタンをレンダリングします。
       <button type="submit" className="mt-4 btn btn-outline btn-success w-full">
         submit
       </button>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,31 @@
 "use client"
+// 必要なフックとコンポーネントをインポート
 import { useState } from 'react';
 import Search from './Search';
 import DisplayImages from './DisplayImages';
 
+// メインのHomeコンポーネント
 export default function Home() {
+  // フェッチしたデータを保存するためのステート
   const [fetchData, setFetchData] = useState([]);
 
+  // 検索とデータのフェッチを処理する関数
   const handleSearch = async (keyword) => {
+    // Pixabay APIのエンドポイントURL
     const endpointURL = `https://pixabay.com/api/?key=${process.env.NEXT_PUBLIC_PIXABAY_API_KEY}&q=${keyword}&image_type=photo`;
-    // const endpointURL = `https://pixabay.com/api/?key=40951011-80d0638d707057db738a74242&q=${keyword}&image_type=photo`;
 
-
+    // APIからデータをフェッチ
     const res = await fetch(endpointURL);
+    // レスポンスをJSONにパース
     const data = await res.json();
+    // 必要なデータを抽出
     const fetchData_raw = data.hits;
 
+    // フェッチしたデータでステートを更新
     setFetchData(fetchData_raw);
   };
 
+  // コンポーネントのレンダリング
   return (
     <div className="container mx-auto">
       <h2 className="text-2xl font-bold mb-4 w-1/2 mx-auto text-center">My pix</h2>
